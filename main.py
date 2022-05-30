443b560365b2a836e9f9d889b98f4d4b7a038ea5
tk_m123 <eee.runi@gmail.com>
2022-05-31 05:52:37 +0900

    2nd commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # from matplotlib.pyplot import text
 import streamlit as st
-# import numpy as np
-# import pandas as pd
-# from PIL import Image
+import numpy as np
+import pandas as pd
+from PIL import Image
 import time
 
 st.title("Streamlit 超入門")
